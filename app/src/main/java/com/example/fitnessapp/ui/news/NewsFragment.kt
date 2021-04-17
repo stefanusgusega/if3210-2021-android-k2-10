f87a8fda4c2207dd51684325204556f798f0e7d1
stefanusgusega <gunawanstefanus006@gmail.com>
2021-04-17 19:39:07 +0700

    adapt to needed value + add dependency retrofit gson recycler

--- app/src/main/java/com/example/fitnessapp/ui/news/NewsFragment.kt
+++ app/src/main/java/com/example/fitnessapp/ui/news/NewsFragment.kt
@@ -1,4 +1,4 @@
-package com.example.fitnessapp.ui.home
+package com.example.fitnessapp.ui.news
 
 import android.os.Bundle
 import android.view.LayoutInflater
@@ -10,20 +10,20 @@ import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import com.example.fitnessapp.R
 
-class HomeFragment : Fragment() {
+class NewsFragment : Fragment() {
 
-    private lateinit var homeViewModel: HomeViewModel
+    private lateinit var newsViewModel: NewsViewModel
 
     override fun onCreateView(
             inflater: LayoutInflater,
             container: ViewGroup?,
             savedInstanceState: Bundle?
     ): View? {
-        homeViewModel =
-                ViewModelProvider(this).get(HomeViewModel::class.java)
-        val root = inflater.inflate(R.layout.fragment_home, container, false)
-        val textView: TextView = root.findViewById(R.id.text_home)
-        homeViewModel.text.observe(viewLifecycleOwner, Observer {
+        newsViewModel =
+                ViewModelProvider(this).get(NewsViewModel::class.java)
+        val root = inflater.inflate(R.layout.fragment_news, container, false)
+        val textView: TextView = root.findViewById(R.id.text_news)
+        newsViewModel.text.observe(viewLifecycleOwner, Observer {
             textView.text = it
         })
         return root
