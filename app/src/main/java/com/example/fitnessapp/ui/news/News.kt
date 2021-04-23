0871e39591644bc18ad0d45fb581ac132163f0f4
stefanusgusega <13518149@std.stei.itb.ac.id>
2021-04-24 01:42:53 +0700

    Add News model and API Interface

---
+++ app/src/main/java/com/example/fitnessapp/ui/news/News.kt
@@ -0,0 +1,9 @@
+package com.example.fitnessapp.ui.news
+
+data class News (
+        val author: String,
+        val title: String,
+        val description: String,
+        val url: String,
+        val urlToImage: String
+)
---
+++ app/src/main/java/com/example/fitnessapp/ui/news/RetrofitClient.kt
@@ -0,0 +1,5 @@
+package com.example.fitnessapp.ui.news
+
+object RetrofitClient {
+    private const val BASE_URL = "https://newsapi.org/v2/"
+}
